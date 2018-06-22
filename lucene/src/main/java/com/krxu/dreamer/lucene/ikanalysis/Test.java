d83179e9d42a9f0872a83ff9d99d79a9852a46d1
xukuairan <15757179829@139.com>
2018-06-22 21:05:59 +0800

    'new'

--- lucene/src/main/java/com/krxu/dreamer/lucene/ikanalysis/Test.java
+++ lucene/src/main/java/com/krxu/dreamer/lucene/ikanalysis/Test.java
@@ -1,6 +1,12 @@
 package com.krxu.dreamer.lucene.ikanalysis;
 
-import java.io.*;
+import com.huaban.analysis.jieba.WordStatistics;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author xukuairan
@@ -9,29 +15,46 @@ import java.io.*;
  */
 public class Test {
 
-    public static void main(String[] args) {
-        File file = new File("D:\\workspace\\idea\\dreamer\\lucene\\src\\main\\resources\\chinese_dict.txt");
-        try {
-            FileOutputStream fos=new FileOutputStream("D:\\workspace\\idea\\dreamer\\lucene\\src\\main\\resources\\chinese_word.dic");
-            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fos);
-            BufferedWriter bw=new BufferedWriter(outputStreamWriter);
+    public static void main(String[] args) throws InterruptedException {
+        String path = "D:\\workspace\\idea\\dreamer\\lucene\\src\\main\\resources\\news.txt";
+        List<Map.Entry<String, Integer>> results = WordStatistics.getWordFrequency(getContent(path), com.krxu.dreamer.textanalysis.WordStatistics.wordLabel);
+        showTopN(results, 3);
+        long a = System.currentTimeMillis();
+        String path2 = "D:\\workspace\\idea\\dreamer\\lucene\\src\\main\\resources\\news2.txt";
+        List<Map.Entry<String, Integer>> results2 = WordStatistics.getWordFrequency(getContent(path2), com.krxu.dreamer.textanalysis.WordStatistics.wordLabel);
+        long b = System.currentTimeMillis();
+        showTopN(results2, 3);
+        List<Map.Entry<String, Integer>> results3 = WordStatistics.getWordFrequency(getContent(path2), com.krxu.dreamer.textanalysis.WordStatistics.wordLabel);
+        long c = System.currentTimeMillis();
+        System.out.println((b - a) + " ... " + (c-b));
 
-            BufferedReader br = new BufferedReader(new FileReader(file));
+
+        while (true){
+            Thread.sleep(1000);
+        }
+    }
+
+
+    public static void showTopN(List<Map.Entry<String, Integer>> results, int N) {
+        for (int i = 0; i < N && i < results.size(); i++) {
+            Map.Entry<String, Integer> result = results.get(i);
+            System.out.println(result.getKey() + "  " + result.getValue());
+        }
+    }
+
+
+    public static String getContent(String path) {
+        File newsFile = new File(path);
+        StringBuilder result = new StringBuilder();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(newsFile));
             String str;
             while ((str = br.readLine()) != null) {
-                System.out.println(str.split(" ")[0]);
-                bw.write(str.split(" ")[0]);
-                bw.newLine();
+                result.append(System.lineSeparator() + str);
             }
             br.close();
-
-
-
         } catch (Exception e) {
-            e.printStackTrace();
         }
-
-
-        System.out.println();
+        return result.toString();
     }
 }
---
+++ lucene/src/main/java/com/krxu/dreamer/textanalysis/WordStatistics.java
@@ -0,0 +1,42 @@
+package com.krxu.dreamer.textanalysis;
+
+import com.huaban.analysis.jieba.JiebaSegmenter;
+
+import java.io.*;
+import java.util.HashSet;
+
+/**
+ * @author xukuairan
+ * @description 添加类描述
+ * @dete ${date}
+ */
+public class WordStatistics {
+
+    /**
+     * 词汇表
+     */
+    public static HashSet<String> wordLabel ;
+
+    static{
+        wordLabel = new HashSet<>();
+        BufferedReader br = null;
+        try {
+            InputStream in =
+                    Thread.currentThread().getContextClassLoader().getResourceAsStream("chinese_word.dic");
+            br = new BufferedReader(new InputStreamReader(in));
+            String str;
+            while ((str = br.readLine()) != null) {
+                wordLabel.add(str);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                br.close();
+            } catch (IOException e) {
+            }
+        }
+        System.out.println("init lebal end ,size : " + wordLabel.size());
+    }
+
+}
