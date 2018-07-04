900345479bde047b51871467a4cd21d8c961bbcf
徐快然 <xukuairan@xukuairandeMacBook-Pro.local>
2018-07-04 16:35:08 +0800

    no

--- basic/src/main/java/com/krxu/dreamer/basic/Application.java
+++ basic/src/main/java/com/krxu/dreamer/basic/Application.java
@@ -1,5 +1,8 @@
 package com.krxu.dreamer.basic;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 /**
  * @author xukuairan
  * @version xxx
@@ -8,4 +11,13 @@ package com.krxu.dreamer.basic;
  */
 public class Application {
 
+    public static void main(String[] args) {
+        int count = 2;
+        int allCount = 11;
+        BigDecimal a = new BigDecimal(count);
+        BigDecimal b = new BigDecimal(allCount);
+        BigDecimal c = a.divide(b, 4, RoundingMode.HALF_UP);
+        System.out.println(c.toString());
+    }
 }
+
