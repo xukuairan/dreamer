19bbdadcd05d61b700110caba3b19e21dd95c6ac
徐快然 <xukuairan@xukuairandeMacBook-Pro.local>
2018-07-28 16:11:41 +0800

    c

--- basic/src/main/java/com/krxu/dreamer/basic/Application.java
+++ basic/src/main/java/com/krxu/dreamer/basic/Application.java
@@ -1,7 +1,6 @@
 package com.krxu.dreamer.basic;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
+import java.util.*;
 
 /**
  * @author xukuairan
@@ -12,8 +11,43 @@ import java.math.RoundingMode;
 public class Application {
 
     public static void main(String[] args) {
-        String str = "你好c";
-        System.out.println(str.length());
+        int[] result = mathAbs(0, 15,14);
+
+        for(int i: result){
+            System.out.println(i);
+        }
     }
+
+    /**
+     * 随机数
+     *
+     * @param min 随机的最小值
+     * @param max 随机的最大值
+     * @param n   随机数个数
+     * @return
+     */
+    public static int[] mathAbs(int min, int max, int n) {
+        int len = max - min + 1;
+
+        //初始化给定范围的待选数组
+        int[] source = new int[len];
+        for (int i = min; i < min + len; i++) {
+            source[i - min] = i;
+        }
+
+        int[] result = new int[n];
+        Random rd = new Random();
+        int index = 0;
+        for (int i = 0; i < result.length; i++) {
+            //待选数组0到(len-2)随机一个下标
+            index = Math.abs(rd.nextInt() % len--);
+            //将随机到的数放入结果集
+            result[i] = source[index];
+            //将待选数组中被随机到的数，用待选数组(len-1)下标对应的数替换
+            source[index] = source[len];
+        }
+        return result;
+    }
+
 }
 
