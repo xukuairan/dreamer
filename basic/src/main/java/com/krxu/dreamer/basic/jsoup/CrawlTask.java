bbb95cf1a75596f3de65a4d7cdd541c43151b640
xukuairan <15757179829@139.com>
2018-08-06 22:59:59 +0800

    jsoupadd

---
+++ basic/src/main/java/com/krxu/dreamer/basic/jsoup/CrawlTask.java
@@ -0,0 +1,12 @@
+package com.krxu.dreamer.basic.jsoup;
+
+/**
+ * @author xukuairan
+ * @version [版本号]
+ * @date 2018/8/6
+ * @description [添加描述]
+ */
+public class CrawlTask {
+    public static void main(String[] args) {
+    }
+}
