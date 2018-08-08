3f8a60abdb0ae0bae0c3cbf6a0d99a156e177200
xukuairan <xkr19930924>
2018-08-08 08:15:04 +0800

    删除华为domain

--- basic/src/main/java/com/krxu/dreamer/basic/http/HttpXml.java
+++ basic/src/main/java/com/krxu/dreamer/basic/http/HttpXml.java
@@ -1,33 +1,22 @@
 package com.krxu.dreamer.basic.http;
 
-import com.huawei.iread.server.domain.request.*;
-import com.huawei.iread.server.domain.response.GetUserInfoResponse;
-import com.huawei.iread.server.domain.response.Response;
 import com.thoughtworks.xstream.XStream;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpStatus;
-import org.apache.http.NameValuePair;
 import org.apache.http.StatusLine;
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.util.EntityUtils;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
-import org.dom4j.DocumentHelper;
-import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
 
-import java.io.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 
 /**
  * @author maywin
@@ -42,14 +31,14 @@ public class HttpXml {
         String url = "http://10.211.95.228:9085/csu/portalengine/miguTokenAuthenticate";
 
 
-        MiguTokenAuthenticateRequest request = new MiguTokenAuthenticateRequest();
-        request.setToken("STnid0000001533264473623XfiB0AoZLxTYRjL4fpJUZ8TeCkMnVbKu");
-        //request.setuSessionid("9518dd9f2eca58489cb4dbeb80154217");
-        GetUserInfoResponse response = (GetUserInfoResponse) sendMessageByPost(url, request);
-        System.out.println(response);
+//        MiguTokenAuthenticateRequest request = new MiguTokenAuthenticateRequest();
+//        request.setToken("STnid0000001533264473623XfiB0AoZLxTYRjL4fpJUZ8TeCkMnVbKu");
+//        //request.setuSessionid("9518dd9f2eca58489cb4dbeb80154217");
+//        GetUserInfoResponse response = (GetUserInfoResponse) sendMessageByPost(url, request);
+//        System.out.println(response);
     }
 
-    public static Response sendMessageByPost(String url, Request request) {
+    public static void sendMessageByPost(String url, Object request) {
         XStream xStream = new XStream();
         String xml = xStream.toXML(request);
 
@@ -89,8 +78,6 @@ public class HttpXml {
                     }
 
                     System.out.println(doc);
-
-                    return (Response) xStream.fromXML(inputStream);
                 }
             } finally {
                 response.close();
@@ -109,7 +96,5 @@ public class HttpXml {
                 e.printStackTrace();
             }
         }
-
-        return null;
     }
 }
