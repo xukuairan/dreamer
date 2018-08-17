4c6081accc4f159344766c930b23dbc90a006b53
xukuairan <15757179829@139.com>
2018-08-17 19:41:11 +0800

    aug9

--- jieba/src/test/java/TestJieba.java
+++ jieba/src/test/java/TestJieba.java
@@ -16,7 +16,11 @@ import java.util.Map;
 public class TestJieba {
     @Test
     public void test() {
-        String text = "孩子哈师大在孩子；；》》】阿萨德】";
+        String text = "本报北京8月16日电  （记者王观）国家外汇管理局16日通报21起外汇违规典型案例，包括恒丰银行温州分行违规办理转口贸易案、汇丰银行北京分行违规办理内保外贷案、建设银行连江支行违规办理个人分拆售付汇案、索尔维生物化学（泰兴）有限公司逃汇案等。\n" +
+                "\n" +
+                "　　其中，2016年1月，恒丰银行温州分行未按规定尽职审核转口贸易真实性，凭企业虚假提单办理转口贸易付汇业务。该行上述行为违反《外汇管理条例》第十二条。根据《外汇管理条例》第四十七条，处以罚没款146.4万元。\n" +
+                "\n" +
+                "　　外汇局表示，2018年以来，外汇局加强外汇市场监管，对各类外汇违法违规行为保持高压态势，不断加大处罚力度，严厉打击虚假、欺骗性交易和非法套利等资金“脱实向虚”行为，维护健康良性市场秩序。";
         System.out.println(analyzeWords(text));
 
     }
@@ -25,7 +29,7 @@ public class TestJieba {
     public static Map<String, Integer> analyzeWords(String text) {
         Map<String, Integer> map = new HashMap<>();
         JiebaSegmenter segmenter = new JiebaSegmenter();
-        List<SegToken> segTokens = segmenter.process(text, JiebaSegmenter.SegMode.INDEX);
+        List<SegToken> segTokens = segmenter.process(text, JiebaSegmenter.SegMode.SEARCH);
         System.out.println(segTokens);
         for (SegToken segToken : segTokens) {
             String word = segToken.word;
